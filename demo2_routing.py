2662d404e801ff5b1cf801f30eeea751203f8819
jhonchen <17368089403.163.com>
2019-05-28 16:34:50 +0800

    路由的参数与请求方式指定&PostMan

--- demo2_routing.py
+++ demo2_routing.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,18 @@ def index():
 def demo1():
     return 'demo1'
 
+# 给路由添加参数  格式就是<参数名>
+# 并且视图函数需要接受这个参数
+@app.route('/user/<int:user_id>')
+def demo2(user_id):
+    return 'demo2 %s' % user_id
+
+
+@app.route('/demo3', methods=['get', 'post'])
+def demo3():
+    return 'demo3 %s' % request.method
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    print(app.url_map)
