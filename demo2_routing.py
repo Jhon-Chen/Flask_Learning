0812f1ec11595486f4834ee981aab7684c577434
jhonchen <17368089403.163.com>
2019-05-28 17:20:19 +0800

    09-JSON数据格式&返回JSON

--- demo2_routing.py
+++ demo2_routing.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -22,6 +24,23 @@ def demo2(user_id):
 @app.route('/demo3', methods=['get', 'post'])
 def demo3():
     return 'demo3 %s' % request.method
+
+
+@app.route('/json')
+def demo4():
+    json_dict = {
+        "name": "gakki",
+        "age": 18,
+        "appearance": "lovely",
+        "user_info": {
+            "height": 169,
+            "hobby": "sleep"}
+    }
+    # 使用json.dumps将字典转换成字符串
+    result = json.dumps(json_dict)
+    # 使用json.loads将json字符串转成字典
+    test_dit = json.loads('{"name": "gakki","age": 18}')
+    return result
 
 
 if __name__ == '__main__':
