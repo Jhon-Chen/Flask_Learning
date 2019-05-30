ee3438b5cf7470f0bdaef22be6b333681accf21d
jhonchen <17368089403.163.com>
2019-05-30 19:15:02 +0800

    请求的参数

--- day_2/demo1_upload.py
+++ day_2/demo1_upload.py
@@ -10,10 +10,25 @@ def index():
 
 @app.route('/upload', methods=['post'])
 def upload():
+    """request.args
+    request.data
+    request.url
+    request.method"""
     file = request.files.get('pic')
     file.save('1.gif')
     return 'success'
 
 
+@app.route('/data', methods=['post'])
+def data():
+    data = request.data
+    print(data)
+    return "OK"
+
+
 if __name__ == '__main__':
     app.run(host='192.168.211.128', debug=True)
+
+    """Address already in use
+    ubuntu : netstat -apn |grep 5000
+    kill -9 pid"""
