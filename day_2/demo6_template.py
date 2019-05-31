97f60584b95d17b1a84dd31a0a368b044722a1fa
jhonchen <17368089403.163.com>
2019-05-31 11:44:25 +0800

    template

---
+++ day_2/demo6_template.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/demo1')
+def demo1():
+    my_int = 10
+    my_str = 'enheng'
+    my_list = [1, 4, 5]
+    my_dict = {
+        'da': 1,
+        'se': 'asd'
+    }
+    return render_template('demo6_template.html',
+                           a=my_int,
+                           b=my_str,
+                           c=my_list,
+                           d=my_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
