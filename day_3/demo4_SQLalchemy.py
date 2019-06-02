9b40e97aba808abbce57f3657144a8eea50b446e
jhonchen <17368089403.163.com>
2019-06-02 21:35:11 +0800

    flask_sqlalchemy 增删改查

--- day_3/demo4_SQLalchemy.py
+++ day_3/demo4_SQLalchemy.py
@@ -4,9 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # 配置并初始化对象
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://username:password@localhost:port/database_name"
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://jhonchen:2553522375@127.0.0.1:3306/flask"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
 
 
 @app.route('/')
@@ -15,4 +20,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
