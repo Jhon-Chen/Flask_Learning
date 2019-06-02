9951757d734ceed3550c89babaf8679e0f27c69a
jhonchen <17368089403.163.com>
2019-06-02 11:40:52 +0800

    WTF表单验证

--- day_3/demo3_WTF.py
+++ day_3/demo3_WTF.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import InputRequired, EqualTo
 app = Flask(__name__)
 app.secret_key = 'qwoeufkljniuc'
+app.config['WTF_CSRF_ENABLED'] = False
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('用户名:', render_kw={'placeholder': '我是占位文字'})
-    password = PasswordField('密码:')
-    password2 = PasswordField('确认密码:')
+    username = StringField('用户名:', validators=[InputRequired('请输入用户名')], render_kw={'placeholder': '我是占位文字'})
+    password = PasswordField('密码:', validators=[InputRequired('请输入密码')])
+    password2 = PasswordField('确认密码:', validators=[InputRequired('请确认密码'), EqualTo('password', '两次密码要一致')])
     submit = SubmitField('注册')
 
 
@@ -21,6 +22,19 @@ def index():
 @app.route('/register_wtf', methods=['POST', 'GET'])
 def register_wtf():
     register_form = RegisterForm()
+    # 使用wtf表单帮我们做验证
+    if register_form.validate_on_submit():
+        # 执行注册逻辑
+        # 取到表单中提交上来的三个参数
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+        # 假装做注册操作
+        print(username, password, password2)
+        return "success"
+    else:
+        if request.method == 'POST':
+            flash('参数错误')
     return render_template('temp5_WTF.html', form=register_form)
 
 
