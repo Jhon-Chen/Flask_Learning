a4b50b1da45c510d70a790faf92faa34fcff4615
jhonchen <17368089403.163.com>
2019-06-14 10:34:50 +0800

    蓝图中的模板和静态文件

--- day_4/demo4_blueprint/cart/__init__.py
+++ day_4/demo4_blueprint/cart/__init__.py
@@ -2,6 +2,6 @@
 from flask import Blueprint
 
 # 初始化蓝图对象
-cart_blu = Blueprint('cart', __name__)
+cart_blu = Blueprint('cart', __name__, static_folder="static", url_prefix='/cart', template_folder='templates')
 
 from .views import *
--- day_4/demo4_blueprint/cart/views.py
+++ day_4/demo4_blueprint/cart/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from . import cart_blu
 
 # 3.创建蓝图路由
-@cart_blu.route('/cart/list')
+@cart_blu.route('/list')
 def cart_list():
-    return "cart_list"
+    return render_template('cart_html.html')
